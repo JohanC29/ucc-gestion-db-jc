e053ada47e43bafa63e4ca4648dd265462f98525
JohanC29 <jc.muelasvernaza@gmail.com>
2022-10-12 20:04:54 -0500

    Se agrega al archivo .bat el comando del pentaho

---
+++ 01.Job/01.Transformaciones/tarea_programada_2.bat
@@ -0,0 +1,3 @@
+echo "Ejecucion desde la tarea programada"
+pan -file:transformada_02.ktr > log.txt
+pause
--- 01.Job/tarea_programada.bat
+++ 01.Job/tarea_programada.bat
@@ -1,3 +1,3 @@
 echo "Ejecucion desde la tarea programada"
-date /t
+pan -file:transformada_01_Intento02.ktr
 pause
